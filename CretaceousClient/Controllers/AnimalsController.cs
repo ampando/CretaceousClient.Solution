57c2ff29a04904df48d97a253c55a58546cccf2f
Adrienne <100542795+ampando@users.noreply.github.com>
2022-08-31 20:09:42 -0700

    create details and edit route and view for animal controller.

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -7,8 +7,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousClient.Models;
 
-public IActionResult Index()
+namespace CretaceousClient.Controllers
 {
-  var allAnimals = Animal.GetAnimals();
-  return View(allAnimals);
+  public class AnimalsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allAnimals = Animal.GetAnimals();
+      return View(allAnimals);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Animal animal)
+    {
+      Animal.Post(animal);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var animal = Animal.GetDetails(id);
+      return View(animal);
+    }
+    
+    public IActionResult Edit(int id)
+    {
+      var animal = Animal.GetDetails(id);
+      return View(animal);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Animal animal)
+    {
+      animal.AnimalId = id;
+      Animal.Put(animal);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Animal.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
 }
