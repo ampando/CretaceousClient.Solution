39d479cdcd50b889bba5a0d17321c088eba31b96
Adrienne <100542795+ampando@users.noreply.github.com>
2022-09-01 13:43:59 -0700

    update apihelper and finalize project.

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using CretaceousClient.Models;
 
 namespace CretaceousClient.Controllers
